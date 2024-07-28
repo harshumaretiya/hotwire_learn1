b67e539583135d7129b0761013310bfbd9865eaa
harshumaretiya <harsh.patel.hp846@gmail.com>
2024-07-28 12:58:49 +0530

    header added

--- app/javascript/controllers/dropdown_controller.js
+++ app/javascript/controllers/dropdown_controller.js
@@ -1,21 +1,33 @@
+// app/javascript/controllers/dropdown_controller.js
 import { Controller } from "@hotwired/stimulus";
 
 export default class extends Controller {
   static targets = ["menu"];
 
-  connect() {
-    this.hideMenu();
-  }
-
   showMenu() {
     this.menuTarget.classList.remove("hidden");
-    this.menuTarget.classList.add("opacity-100", "scale-100");
-    this.menuTarget.classList.remove("opacity-0", "scale-95");
+    this.menuTarget.classList.add("block");
+    this.menuTarget.classList.add("opacity-100");
+    this.menuTarget.classList.remove("opacity-0");
+    this.menuTarget.classList.add("scale-100");
+    this.menuTarget.classList.remove("scale-95");
   }
 
   hideMenu() {
     this.menuTarget.classList.add("hidden");
-    this.menuTarget.classList.remove("opacity-100", "scale-100");
-    this.menuTarget.classList.add("opacity-0", "scale-95");
+    this.menuTarget.classList.remove("block");
+    this.menuTarget.classList.add("opacity-0");
+    this.menuTarget.classList.remove("opacity-100");
+    this.menuTarget.classList.add("scale-95");
+    this.menuTarget.classList.remove("scale-100");
+  }
+
+  toggleMenu() {
+    this.menuTarget.classList.toggle("hidden");
+    this.menuTarget.classList.toggle("block");
+    this.menuTarget.classList.toggle("opacity-100");
+    this.menuTarget.classList.toggle("opacity-0");
+    this.menuTarget.classList.toggle("scale-100");
+    this.menuTarget.classList.toggle("scale-95");
   }
 }
